e3195512e3a9389f4924a81d554eebacbf08d6e6
Ahmed_barakat27 <ab560968@gmail.com>
2025-01-02 20:36:30 +0200

    fi

--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -17,7 +17,7 @@ async function getData(productId: string) {
 export default async function EditProduct({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const data = await getData((await params).id);
 
