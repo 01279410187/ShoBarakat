c2891194e40290185f4f9728721f59f5e3823ea4
Ahmed_barakat27 <ab560968@gmail.com>
2025-01-03 19:24:05 +0200

    ffui

--- app/dashboard/products/[id]/page.tsx
+++ app/dashboard/products/[id]/page.tsx
@@ -1,3 +1,32 @@
+// import { EditForm } from "@/components/ui/dashboard/EditForm";
+// import prisma from "@/lib/db";
+// import { notFound } from "next/navigation";
+
+// async function getData(productId: string) {
+//   const data = await prisma.product.findUnique({
+//     where: {
+//       id: productId,
+//     },
+//   });
+//   if (!data) {
+//     return notFound();
+//   }
+//   return data;
+// }
+
+// export default async function EditProduct({
+//   params,
+// }: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const data = await getData((await params).id);
+
+//   return <EditForm data={data} />;
+// }
+
+
+
+
 import { EditForm } from "@/components/ui/dashboard/EditForm";
 import prisma from "@/lib/db";
 import { notFound } from "next/navigation";
@@ -17,9 +46,9 @@ async function getData(productId: string) {
 export default async function EditProduct({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string }; // Corrected: params is a plain object, not a Promise
 }) {
-  const data = await getData((await params).id);
+  const data = await getData(params.id); // Directly access params.id
 
   return <EditForm data={data} />;
 }
